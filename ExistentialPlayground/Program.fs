1c78a62b01f8cffe4755bc9d0bf2afe13d4b5260
Jack Matusiewicz <jack.matusiewicz@gmail.com>
2018-08-05 19:57:04 +0100

    Adding h-tree example

--- ExistentialPlayground/Program.fs
+++ ExistentialPlayground/Program.fs
@@ -3,5 +3,15 @@ open ExistentialPlayground
 
 [<EntryPoint>]
 let main argv =
-    printfn "TODO"
+
+    let four = TreeCata.makeLeaf 4
+    let five = TreeCata.makeLeaf 5
+    let nine = TreeCata.makeLeaf 9
+    let c = TreeCata.makeLeaf 'c'
+
+    let tree = Branch (Branch (four, c), Branch(five, Branch(nine, Empty)))
+    let t2 = TreeCata.cata TreeCata.identityCata tree
+    printfn "%s" <| TreeCata.cata TreeCata.stringCata tree
+    printfn "%s" <| TreeCata.cata TreeCata.stringCata t2
+
     0
--- ExistentialPlayground/TreeCata.fs
+++ ExistentialPlayground/TreeCata.fs
@@ -18,12 +18,34 @@ type TreeCata<'r> =
 
 module TreeCata =
 
+    let makeLeaf (x : 'a) =
+        { new LeafCrate with
+            member __.Bind evalr = evalr.Eval x
+        } |> Leaf
+
     let identityCata<'a> : TreeCata<'a Tree> =
         { new TreeCata<_> with
             member __.Empty () = Empty
-            member __.Leaf x =
-                { new LeafCrate with
-                    member __.Bind evalr = evalr.Eval x
-                } |> Leaf
+            member __.Leaf x = makeLeaf x
             member __.Branch l r = Branch (l,r)
         }
+
+    let stringCata : TreeCata<string> =
+        { new TreeCata<_> with
+            member __.Empty () = "Empty"
+            member __.Leaf x = x.ToString ()
+            member __.Branch l r = sprintf "BRANCH (%s. %s)" l r
+        }
+
+    let rec cata (c : TreeCata<'a>) (tree : 'b Tree) : 'a =
+        match tree with
+        | Empty -> c.Empty ()
+        | Leaf x ->
+            x.Bind
+                { new LeafEvaluator<_> with
+                    member __.Eval a = c.Leaf a
+                }
+        | Branch (l,r) ->
+            let l = cata c l
+            let r = cata c r
+            c.Branch l r
