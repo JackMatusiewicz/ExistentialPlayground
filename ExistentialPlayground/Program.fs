53021354b074ea2665651b71f9f45f8cf5db8251
Jack Matusiewicz <Jack.Matusiewicz@outlook.com>
2018-08-19 11:19:23 +0100

    Adding test code

--- ExistentialPlayground/Program.fs
+++ ExistentialPlayground/Program.fs
@@ -4,14 +4,12 @@ open ExistentialPlayground
 [<EntryPoint>]
 let main argv =
 
-    let four = TreeCata.makeLeaf 4
-    let five = TreeCata.makeLeaf 5
-    let nine = TreeCata.makeLeaf 9
-    let c = TreeCata.makeLeaf 'c'
+    let lst : HList<int -> string -> char -> unit> =
+        HList.empty
+        |> HList.cons 'c'
+        |> HList.cons "Hello"
+        |> HList.cons 25
 
-    let tree = Branch (Branch (four, c), Branch(five, Branch(nine, Empty)))
-    let t2 = TreeCata.cata TreeCata.identity tree
-    printfn "%s" <| TreeCata.cata TreeCata.toString tree
-    printfn "%s" <| TreeCata.cata TreeCata.toString t2
+    printfn "%A" <| lst.GetType().ToString()
 
     0
