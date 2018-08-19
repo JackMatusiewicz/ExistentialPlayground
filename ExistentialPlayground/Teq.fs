51e58d821e590ef311b48cd56d9a7bf4d8584c61
Jack Matusiewicz <Jack.Matusiewicz@outlook.com>
2018-08-19 11:03:29 +0100

    Adding Teq

---
+++ ExistentialPlayground/Teq.fs
@@ -0,0 +1,10 @@
+﻿namespace ExistentialPlayground
+
+type Teq<'a, 'b> = private Teq of ('a -> 'b) * ('b -> 'a)
+
+module Teq =
+
+    let refl<'a> : Teq<'a, 'a> = Teq (id, id)
+
+    let castTo (Teq (f,g)) b = g b
+    let castFrom (Teq (f,g)) a = f a
