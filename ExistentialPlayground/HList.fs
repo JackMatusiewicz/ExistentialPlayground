3ef08695485283b88d5c26ce608262a51530f87b
Jack Matusiewicz <Jack.Matusiewicz@outlook.com>
2018-08-19 11:06:54 +0100

    Adding HList type

---
+++ ExistentialPlayground/HList.fs
@@ -0,0 +1,11 @@
+﻿namespace ExistentialPlayground
+
+type HList<'a> =
+    | End of Teq<'a, unit>
+    | Cons of HListCrate<'a>
+
+and HListCrate<'a> =
+    abstract member Bind<'r> : HListEvaluator<'a, 'r> -> 'r
+
+and HListEvaluator<'a, 'r> =
+    abstract member Eval<'b, 'c> : 'b -> 'c HList -> Teq<'a, 'b -> 'c> -> 'r
