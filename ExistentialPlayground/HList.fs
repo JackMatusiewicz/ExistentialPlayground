6b29052eac4898a5ece5a28f7da1307d69fed02f
Jack Matusiewicz <Jack.Matusiewicz@outlook.com>
2018-08-19 11:16:28 +0100

    Adding HList creation functions

--- ExistentialPlayground/HList.fs
+++ ExistentialPlayground/HList.fs
@@ -9,3 +9,13 @@ and HListCrate<'a> =
 
 and HListEvaluator<'a, 'r> =
     abstract member Eval<'b, 'c> : 'b -> 'c HList -> Teq<'a, 'b -> 'c> -> 'r
+
+module HList =
+
+    let empty : HList<unit> = Teq.refl<unit> |> End
+
+    let cons (v : 'a) (tail : HList<'b>) : HList<'a -> 'b> =
+        { new HListCrate<'a -> 'b> with
+            member __.Bind<'r> (evaluator : HListEvaluator<('a -> 'b), 'r>) =
+                evaluator.Eval<'a, 'b> v tail Teq.refl<'a -> 'b>
+        } |> Cons
