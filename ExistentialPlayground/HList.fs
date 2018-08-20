a8ac61a0d2383fec1c249011b60feb299f458e45
Jack Matusiewicz <Jack.Matusiewicz@outlook.com>
2018-08-20 09:41:42 +0100

    Adding the head function for HList

--- ExistentialPlayground/HList.fs
+++ ExistentialPlayground/HList.fs
@@ -8,14 +8,29 @@ and HListCrate<'a> =
     abstract member Bind<'r> : HListEvaluator<'a, 'r> -> 'r
 
 and HListEvaluator<'a, 'r> =
-    abstract member Eval<'b, 'c> : 'b -> 'c HList -> Teq<'a, 'b -> 'c> -> 'r
+    abstract member Eval : 'b -> 'c HList -> Teq<'a, 'b -> 'c> -> 'r
 
 module HList =
+
+    let private lift (t : Teq<'a, 'b>) : Teq<HList<'a>, HList<'b>> =
+        unbox Teq.believeMe
 
     let empty : HList<unit> = Teq.refl<unit> |> End
 
     let cons (v : 'a) (tail : HList<'b>) : HList<'a -> 'b> =
         { new HListCrate<'a -> 'b> with
             member __.Bind<'r> (evaluator : HListEvaluator<('a -> 'b), 'r>) =
-                evaluator.Eval<'a, 'b> v tail Teq.refl<'a -> 'b>
+                evaluator.Eval v tail Teq.refl<'a -> 'b>
         } |> Cons
+
+    let head<'a, 'b> (h : HList<'a -> 'b>) : 'a * HList<'b> =
+        match h with
+        | End _ -> failwith "Impossible"
+        | Cons h ->
+            h.Bind<'a * HList<'b>>
+                { new HListEvaluator<'a -> 'b, 'a * HList<'b>> with
+                    member __.Eval v tail teq =
+                        let t2 = Teq.domain teq
+                        let t3 = Teq.codomain teq |> lift
+                        (v |> Teq.castTo t2), (tail |> Teq.castTo t3)
+                }
--- ExistentialPlayground/Teq.fs
+++ ExistentialPlayground/Teq.fs
@@ -6,5 +6,10 @@ module Teq =
 
     let refl<'a> : Teq<'a, 'a> = Teq (id, id)
 
+    let believeMe<'a, 'b> : Teq<'a, 'b> = unbox refl<'a>
+
     let castTo (Teq (f,g)) b = g b
     let castFrom (Teq (f,g)) a = f a
+
+    let domain (_ : Teq<'a -> 'b, 'c -> 'd>) : Teq<'a, 'c> = believeMe
+    let codomain (_ : Teq<'a -> 'b, 'c -> 'd>) : Teq<'b, 'd> = believeMe
