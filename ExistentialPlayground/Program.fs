533d3db0de27557e7e8bd1d626267c216a19133c
Jack Matusiewicz <Jack.Matusiewicz@outlook.com>
2018-08-23 11:08:30 +0100

    Adding helper functions

--- ExistentialPlayground/Program.fs
+++ ExistentialPlayground/Program.fs
@@ -2,19 +2,22 @@
 
 let makeTuple (a : 'a) (b : 'b) (c : 'c) = (a,b,c)
 
+let uncurry (f : 'a -> 'b -> 'c) (a,b) = f a b
+
 [<EntryPoint>]
 let main argv =
 
-    let lst : HList<int -> string -> char -> unit> =
+    let lst : HList<_> =
         HList.empty
         |> HList.cons 'c'
         |> HList.cons "Hello"
         |> HList.cons 25
+        |> HList.cons 25.05
 
     let x =
         HList.apply<_,_,_> makeTuple lst
-        |> (fun (f,t) -> HList.apply<_,_,_> f t)
-        |> (fun (f,t) -> HList.apply<_,_,_> f t)
+        |> (uncurry HList.apply<_,_,_>)
+        |> (uncurry HList.apply<_,_,_>)
 
     printfn "%A" (fst x)
     0
