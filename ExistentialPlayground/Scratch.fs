956a881982e437bd0bdcd3c826bd3cbbf004bf76
Jack Matusiewicz <jack.matusiewicz@gmail.com>
2018-09-12 21:02:51 +0100

    Adding FooHList

---
+++ ExistentialPlayground/Scratch.fs
@@ -0,0 +1,38 @@
+﻿namespace ExistentialPlayground
+
+type 'a Foo = unit -> 'a
+
+// An HList where we can guarantee that everything inside it is a Foo.
+type FooHList<'a> =
+    | FEnd of Teq<'a, unit>
+    | FCons of FooHListCrate<'a>
+
+and FooHListCrate<'a> =
+    abstract member Apply<'r> : FooHListEvaluator<'a, 'r> -> 'r
+
+and FooHListEvaluator<'a, 'r> =
+    abstract member Eval<'b, 'c> : Foo<'b> -> FooHList<'c> -> Teq<'a, 'b -> 'c> -> 'r
+
+module FooHList =
+
+    let empty = FEnd Teq.refl
+
+    let lift (_ : Teq<'a, 'b>) : Teq<'a FooHList, 'b FooHList> =
+        Teq.believeMe
+
+    let cons (head : Foo<'a>) (tail : FooHList<'b>) : FooHList<'a -> 'b>
+        =
+        { new FooHListCrate<'a -> 'b> with
+            member __.Apply<'r> (evaluator : FooHListEvaluator<('a -> 'b), 'r>) =
+                evaluator.Eval<'a, 'b> head tail Teq.refl
+        } |> FCons
+
+    let head (a : ('a -> 'b) FooHList) : 'a * ('b FooHList) =
+        match a with
+        | FEnd _ -> failwith "This is impossible, we have a proof that the HList contains at least one element"
+        | FCons c ->
+            c.Apply<'a * ('b FooHList)>
+                { new FooHListEvaluator<'a -> 'b, 'a * ('b FooHList)> with
+                    member __.Eval<'a, 'b> aFoo bTail teq =
+                        failwith "todo"
+                }
---
+++ ExistentialPlayground/TypeLeak.fs
@@ -0,0 +1,26 @@
+﻿module TypeLeak
+
+type SimpleEvaluator<'r> =
+    abstract member Eval<'a> : 'a list -> 'r
+type SimpleCrate =
+    abstract member Apply<'r> : SimpleEvaluator<'r> -> 'r
+
+
+type SimpleDu =
+    | Empty
+    | Single of SimpleCrate
+    | Double of SimpleCrate
+
+//You should be able able to have both of these parameters of type 'a list, as they are different cases.
+//However, the type from the above case has leaked into the lower - we can solve this with explicit <'a> on eval
+let print (sc : SimpleDu) =
+    match sc with
+    | Empty -> "empty"
+    | Single s ->
+        s.Apply { new SimpleEvaluator<_> with
+            member __.Eval<'a> (l : 'a list) = sprintf "%A" l
+        }
+    | Double sc ->
+        sc.Apply { new SimpleEvaluator<_> with
+            member __.Eval<'a> (l : 'a list) = sprintf "%A" l
+        }
